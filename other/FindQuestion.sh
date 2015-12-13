d0e7e87b3770911a2dd3897bb9cdc29c61fa8233
oiraMm <mario.moulin@etu.udamail.fr>
2015-12-13 19:20:22 +0100

    Avancement de FindQuestion.sh et petite modif temporaire sur la librairie dans parseQuestionFile correction(2)

--- other/FindQuestion.sh
+++ other/FindQuestion.sh
@@ -1,29 +1,45 @@
 #! /bin/bash
 
+source EvalLib.sh
+
 # Si le module n'existe pas
-if test -z $MODULE then 
-	echo "Module inconnu" >&2
+if test -z $MODULE; then 
+	echo "FindQuestion : Module inconnu" >&2
 	exit 1
 fi
 
 # Si le cours ou les cours n'existe pas
-if test -z $COURSES then
+if test -z $COURSES; then
 	echo "Cours inconnu" >&2
 	exit 2
 fi
 
-#Variables env definies: Module et Courses
+#Si la variable environnement "KEYWORDS" n'est pas définie
 
-while read 
+if test -z $KEYWORDS; then
+	echo "FindQuestion : KEYWORDS non definie !" >&2
+	exit 3
+fi
 
 
+#Variables env definies: MODULE , COURSES et KEYWORDS
 
-#courseData=`cat $COURSESPATH/$.txt`
-#
-#
-#if test $? -ne 0; then
-#        echo "ShowQuestion: Le fichier de la question $QUESTIONID n'existe pas" >&2
-#        exit 3
-#fi
+for keyword  in $KEYWORDS; do
+	echo $keyword
+	for question in `ls $QUESTIONPATH`; do
+		if [ -f "$QUESTIONPATH/$question" ]; then
+			
+  			QKeywords=`parseQuestionFile "keywords" $question`
+			for Qkeyword in $QKeywords; do
+				if [ $Qkeyword = $keyword ] ; then
+					echo $question
+					echo $Qkeyword
+				fi 
+			done 
+   		fi
+		
+	done
+done
+
 
 
