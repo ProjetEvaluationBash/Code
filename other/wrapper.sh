892fa443288f29cb45a8cc8dada0b58b4dd34c0a
pipic1 <pierre.pic63122@gmail.com>
2015-12-14 08:30:34 +0100

    Ajout de Wrapper.sh

---
+++ other/wrapper.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Detail du script: WRAPPER
+# Script permettant d'ajouter une ou plusieurs variable d'environnement 
+# et execute un script associer
+#
+
+## Fonction: err_arg()
+## Detail: Fonction permettant d'afficher une erreur  concernant le nombre d'argument survenue au lancement du programme
+## Argument: aucun
+## Retour: Quitte le programme si erreur et afficher sur la sortie d'erreur
+err_nbarg(){
+	echo "Error: Probleme d'argument (nombre d'argument non valide)">&2
+	exit 1;
+}
+
+set -x
+
+while [ $# -gt 1 ]; do
+	eval "export $1" || err_nbarg
+	shift
+done
+
+$1
+
+
+
