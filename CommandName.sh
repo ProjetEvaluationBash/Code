174fe6762db6d984ad539a101c1c1a35fa3531b7
Liam Jack <cuonic@cuonic.com>
2015-12-14 10:39:33 +0100

    Ajout des bases de Question et CommandName

---
+++ CommandName.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+addQuestion() {
+	# Saisir la question
+	echo "Question:"
+	read question
+
+	# Validation de la question
+	while test "${#question}" -lt 5 -o "${#question}" -gt 512; do
+		echo "Question invalide. Resaisir la question:"
+		read question
+	done
+
+	# Saisir la reponse
+	echo "Reponse:"
+	read answer
+
+	# Validation de la reponse
+        while test "${#question}" -lt 5 -o "${#question}" -gt 512; do
+                echo "Question invalide. Resaisir la question:"
+                read question
+        done
+
+	echo "$question\n$answer"
+	return 0		
+}
