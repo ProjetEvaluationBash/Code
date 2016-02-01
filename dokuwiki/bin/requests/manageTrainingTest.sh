0ee02fb33fbd54074686229a3fd2e526abd3795e
oiraMm <mario.moulin@etu.udamail.fr>
2016-02-01 17:16:21 +0100

    test manageTrainigTest

--- dokuwiki/bin/requests/manageTrainingTest.sh
+++ dokuwiki/bin/requests/manageTrainingTest.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 
 runRequest() {
-	local dokuName=manage_training_test
+	local dokuName=manage_training
 	local out=$DOKU_USERS_DIR/$DokuUser/$dokuName.txt
 	local module=$(param module)
 
 	cat << EOF > $out
-===== Générer un test a propros du module $module =====
+====== Générer un test a propros du module $module ======
 
 	* [[$DOKU_CGI?module=$module&action=createTest|Générer mon test]]
 
 EOF
-	
+		
 	redirect users:$DokuUser:$dokuName
 
 }
