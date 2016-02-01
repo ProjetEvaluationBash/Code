9139c5db8f3fd54a145e20a49cb440d8aa0fae37
pipic1 <pierre.pic63122@gmail.com>
2016-02-01 10:38:36 +0100

    Modification de consultMark.sh

--- dokuwiki/bin/requests/consultMark.sh
+++ dokuwiki/bin/requests/consultMark.sh
@@ -2,17 +2,15 @@
 
 runRequest() {
         cat << EOF > $DOKU_USERS_DIR/$DokuUser/all_notes.txt
-===== Execution avec retour dans une iframe (module $(param module)) =====
+===== Consultation des notes =====
 
 <html>
 <h3>Consulter les notes de $DokuUser:</h3>
 <br>
-<br>
-<iframe name="output1" width="90%" height="250" frameborder="1"></iframe>
 </html>
 EOF
 
 	cgiHeader
-	redirect users:$DokuUser:consult_mark
+	redirect users:$DokuUser:all_notes
 }
 
