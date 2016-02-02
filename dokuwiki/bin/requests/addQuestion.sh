9ba31c71733a13fa6e1ed9d58cea8915c8fff634
Liam Jack <cuonic@cuonic.com>
2016-02-02 23:54:10 +0100

    Ajout de quelques verifications lors de l'ajout de la question

--- dokuwiki/bin/requests/addQuestion.sh
+++ dokuwiki/bin/requests/addQuestion.sh
@@ -15,8 +15,28 @@ runRequest() {
     local question=$(param question)
     local duration=$(param duration)
     local difficulty=$(param difficulty)
-    local visibility=$(param difficulty)
+    local visibility=$(param visibility)
     local type=$(param type)
+
+    if test ! includeSubType $type; then
+        dokuError "Type de question invalide !"
+    fi
+
+    if test "$question" = ""; then
+        dokuError "Le champ question est vide."
+    fi
+
+    if test $difficulty -lt 1 -o $difficulty -gt 3; then
+        dokuError "Difficulté invalide."
+    fi
+
+    if test "$visibility" != "hidden" -a "$visibility" != "training" -a "$visibility" != "exam"; then
+        dokuError "Visibilité invalide."
+    fi
+
+    if test $duration -lt 0 -o $duration -gt 120; then
+        dokuError "Durée invalide."
+    fi
 
     cat << EOF > $out
 ====== addQuestionForm Debug ======
@@ -28,4 +48,29 @@ Visibility: $visibility
 Type: $type
 
 EOF
+}
+
+function includeSubType() {
+    case $1 in
+                'mcq')
+                    source "$CODE_DIR/MCQ.sh"
+                    ;;
+                'commandname')
+                    source "$CODE_DIR/CommandName.sh"
+                    ;;
+                'compoundcommand')
+                    source "$CODE_DIR/CompoundCommand.sh"
+                    ;;
+                'freequestion')
+                    source "$CODE_DIR/FreeQuestion.sh"
+                    ;;
+                'script')
+                    source "$CODE_DIR/Script.sh"
+                    ;;
+                *)
+                    return 1
+                    ;;
+        esac
+
+    return 0
 }
