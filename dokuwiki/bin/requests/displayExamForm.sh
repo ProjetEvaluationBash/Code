aa9ebd6e79d8f9db2f02ed38247cd4703ac1f5ec
pipic1 <pipic1@desktop.home>
2016-02-04 17:12:33 +0100

    test

---
+++ dokuwiki/bin/requests/displayExamForm.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+runRequest() {
+	local dokuName=displayExamForm
+    local out=$DOKU_USERS_DIR/$DokuUser/$dokuName.txt
+	local module=$(param module)
+	local exam=$(param exam)
+
+	local examsDir=$DB_USERS_DIR/$DokuUser/$module/exams
+
+    if [ ! -e $examsDir/$exam ]; then
+            dokuError "L'examen $exam n'existe pas !"
+            return 1
+    fi
+
+	list="$(cat $examsDir/$exam/list)"
+
+	cat << EOF > $out
+====== Affichage d'un examen (module $module) ======
+		<html>
+		Liste des question de l entrainement $exam:
+		</html>
+
+		EOF
+		        redirect users:$DokuUser:$dokuName
+}
