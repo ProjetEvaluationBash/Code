47bf604a978981ae8ed3d0301699c9f4d1733645
oiraMm <mario.moulin@etu.udamail.fr>
2016-02-04 15:13:49 +0100

    test

--- dokuwiki/bin/requests/manageTrainingTest.sh
+++ dokuwiki/bin/requests/manageTrainingTest.sh
@@ -6,8 +6,7 @@ runRequest() {
 	local module=$(param module)
 
 	cat << EOF > $out
-====== Générer un test a propros du module $module ======
-	
+====== Générer un test a propros du module $module =====
 	* [[$DOKU_CGI?module=$module&action=createTest| Générer mon test]]
 EOF
 	cgiHeader		
