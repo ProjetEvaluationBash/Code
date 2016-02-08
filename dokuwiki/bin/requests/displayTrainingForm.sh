8d106c7b0f578790ef9159dd9c87bc503c2b482f
Pierre PIC <pipic1@fraise.u-clermont1.fr>
2016-02-08 09:51:52 +0100

    modification affichage d'un entrainement

--- dokuwiki/bin/requests/displayTrainingForm.sh
+++ dokuwiki/bin/requests/displayTrainingForm.sh
@@ -9,26 +9,29 @@ runRequest() {
 	local testDir=$DB_USERS_DIR/$DokuUser/$module/tests
 	local dokuUserQuestionsDir=$DOKU_USERS_DIR/$DokuUser/questions
 
-    if [ ! -e $testDir/$test ]; then
-            dokuError "L'examen $exam n'existe pas !"
-            return 1
-    fi
+ 	if [ ! -e $testDir/$test ]; then
+		    dokuError "L'examen $exam n'existe pas !"
+		    return 1
+    	fi
 
 	list="$(cat $testDir/$test/list)"
 
 	cat << EOF > $out
-====== Entrainement: $test (module $module) ======
-<html>
-</html>
-EOF
+		====== Entrainement: $test (module $module) ======
+		<html>
+		</html>
+	EOF
 
 	source "$CODE_DIR/Question.sh"
 	QUESTIONPATH="$testDir/$test/questions"
-
-	for i in $list; do
+	J=1
+	for i in $list; do	
+		echo "=== Question $j ==="
+		j=$(($j + 1))
 		QUESTIONID=$i
 		mainLoadQuestion
 		mainShowQuestion >> $out
+		
 	done
 
 	echo "<html><center><form name=\"myForm\" action=\"$DOKU_CGI\" method=\"POST\"><input type=\"submit\" value=\"Valider mon test\"></form></center></html>" >> $out
