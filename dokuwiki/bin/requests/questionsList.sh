fbd603c6bbb997049870cd5ec5be0d863b39404c
Liam Jack <cuonic@cuonic.com>
2016-02-08 09:06:25 +0100

    tentative de recuperation de questionsList.sh

--- dokuwiki/bin/requests/questionsList.sh
+++ dokuwiki/bin/requests/questionsList.sh
@@ -3,47 +3,38 @@
 runRequest() {
 	local dokuName=questions_list
 	local out=$DOKU_USERS_DIR/$DokuUser/$dokuName.txt
-        local module=$(param module)
+    local module=$(param module)
 
 	local dbQuestionsDir=$DB_MODULES_DIR/$module/questions
 	local dokuUserQuestionsDir=$DOKU_USERS_DIR/$DokuUser/questions
 
-        if ! userIsProf; then
-                cgiHeader
-                redirect users:$DokuUser:permissionDenied
-                return 1
-        fi
+    if userIsProf; then
+            redirect users:$DokuUser:permissionDenied
+            return 1
+    fi
 
-        cat << EOF > $out
-====== Liste des questions du module $module ======
-Test1
-EOF
-
+	echo "====== Liste des questions du module $module ======" > $out
 	
 	# Inclure Question.sh
 	source "$CODE_DIR/Question.sh"
+	
+    # Importe les questions (liens symboliques)
 
-	echo "Test2" >> $out
-        # Importe les questions (liens symboliques)
-
-        rm -Rf $dokuUserQuestionsDir
-        mkdir $dokuUserQuestionsDir
-        for i in $(cd $dbQuestionsDir; ls *.txt); do
-                ln -sf $dbQuestionsDir/$i $dokuUserQuestionsDir/$i
-        done
+    rm -Rf $dokuUserQuestionsDir
+    mkdir $dokuUserQuestionsDir
+    for i in $(cd $dbQuestionsDir; ls *.txt); do
+            ln -sf $dbQuestionsDir/$i $dokuUserQuestionsDir/$i
+    done
 
 	QUESTIONPATH=$dokuUserQuestionsDir
 
-        for i in $(cd $dokuUserQuestionsDir; ls *.txt | sed -re 's/\.txt$//' | sort -n); do	
+    for i in $(cd $dokuUserQuestionsDir; ls *.txt | sed -re 's/\.txt$//' | sort -n); do	
 		QUESTIONID=$i
 		
 		mainLoadQuestion
 		mainShowQuestion >> $out
-                #showQuestionItem $dokuUserQuestionsDir/$i.txt
-        done
+    done
 
-        cgiHeader
 	redirect users:$DokuUser:$dokuName
-
 }
 
