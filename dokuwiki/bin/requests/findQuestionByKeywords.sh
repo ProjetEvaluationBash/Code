b0df065ff7ad8e056d755180176f6538301d3cd8
grand <jegrand5@fraise.u-clermont1.fr>
2016-02-14 17:36:30 +0100

    findQuestionByKeywords

--- dokuwiki/bin/requests/findQuestionByKeywords.sh
+++ dokuwiki/bin/requests/findQuestionByKeywords.sh
@@ -7,7 +7,7 @@ runRequest() {
 	local name=$(param name)
 	local list=$(param list)
 	local keyword=$(param keyword)
-	local tab=$(param tab)	
+		
 
 	if userIsProf;then
 		dokuError"Désolé, fonction réservée aux enseignants !"
@@ -16,10 +16,12 @@ runRequest() {
 	name=$DokuUser-$name
 	
 	
+	mkdir -p $examsDir/$name
+	mkdir $examsDir/$name/questionsFind	
 	for i in $list; do 
 		local keywords=$(getQuestionElement $DB_MODULES_DIR/$module/questions$i.txt keywords)
 		if ["$keywords" == $keyword ]
-			tab[i] = $DB_MODULES_DIR/$module/questions/$i.txt
+			cp $DB_MODULES_DIR/$module/questions/$i.txt $examsDir/$name/questionsFind
 		fi
 	done
 			
@@ -30,6 +32,14 @@ runRequest() {
 ======= Recherche de questions (module $module) ======
 
 EOF
-	* Mots clés : $tab
-	redirect users:$DokuUser:modules
+
+	local list="$(ls $examsDir/$name/questionsFind)"
+	if [ -z "$list" ]; then
+		echo "Aucunes questions trouvées !" >> $out
+	else
+		for i in $list; do
+			echo $examsDir/$name/questionsFind 
+		done
+	fi		
+	redirect users:$DokuUser:$dokuName
 }
