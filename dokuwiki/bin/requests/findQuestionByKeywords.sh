b6f8fa596342461c8ea32f7aa917b7371b84acf5
Liam JACK <liam.jack@etu.udamail.fr>
2016-02-14 19:01:30 +0100

    Reparation erreur 500

--- dokuwiki/bin/requests/findQuestionByKeywords.sh
+++ dokuwiki/bin/requests/findQuestionByKeywords.sh
@@ -4,24 +4,25 @@ runRequest() {
 	local dokuName=find_question_by_keywords
 	local out=$DOKU_USERS_DIR/$DokuUser/$dokuName.txt
 	local module=$(param module)
-	local name=$(param name)
+	#local name=$(param name)
 	local list=$(param list)
-	local keyword=$(param keyword)
+	#local keyword=$(param keyword)
 		
 
-	if userIsProf;then
+	if userIsProf; then
 		dokuError"Désolé, fonction réservée aux enseignants !"
 	fi
 
-	name=$DokuUser-$name
+	name="$DokuUser-$name"
 	
 	
-	mkdir -p $examsDir/$name
-	mkdir $examsDir/$name/questionsFind	
+	mkdir -p "$examsDir/$name"
+	mkdir "$examsDir/$name/questionsFind"	
+	
 	for i in $list; do 
 		local keywords=$(getQuestionElement $DB_MODULES_DIR/$module/questions$i.txt keywords)
-		if ["$keywords" == $keyword ]
-			cp $DB_MODULES_DIR/$module/questions/$i.txt $examsDir/$name/questionsFind
+		if test "$keywords" == "$keyword"; then
+			cp "$DB_MODULES_DIR/$module/questions/$i.txt" "$examsDir/$name/questionsFind"
 		fi
 	done
 			
@@ -34,7 +35,7 @@ runRequest() {
 EOF
 
 	local list="$(ls $examsDir/$name/questionsFind)"
-	if [ -z "$list" ]; then
+	if test -z "$list"; then
 		echo "Aucunes questions trouvées !" >> $out
 	else
 		for i in $list; do
