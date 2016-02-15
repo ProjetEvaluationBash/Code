222fca8496d9e1ebfaedcf5a15b4327e3f6a5259
Liam JACK <liam.jack@etu.udamail.fr>
2016-02-15 11:51:20 +0100

    nettoyage de doExam

--- dokuwiki/bin/requests/doExam.sh
+++ dokuwiki/bin/requests/doExam.sh
@@ -2,7 +2,7 @@
 
 runRequest(){
 
-	local dokuName=do_exam
+	
         local out=$DOKU_USERS_DIR/$DokuUser/$dokuName.txt
         local module=$(param module)
 	local name=$(param name)
@@ -15,31 +15,20 @@ runRequest(){
 	for prof in $profs; do
 		if [ -e $profsDir/$prof/$module/exams ]; then	
 			curentProfExamDir=$profsDir/$prof/$module/exams
-			listExams="`cd $curentProfExamDir; ls -d */`"
-			tmpName="$name/"
-			for exam in $listExams; do
-				if [ "$exam" == "$tmpName" ]; then
-					cp -r  $curentProfExamDir/$exam $examsDir
-				fi
-			done
+
+			if [ -e $curentProfExamDir/$name ];then
+				cp -r  $curentProfExamDir/$exam $examsDir
+				break
+			fi
 		fi
 	done
-
 	
-cat << EOF > $out
-
-====== Examen du module : $module ======
-
-exam : $exam
-name : $name
-tmpname : $tmpName
-profs :$profs
-list exam : $listExams
-current prof exam dir : $curentProfExamDir
-EOF
-
-
-        cgiHeader
+	if [ ! -e $examsDir/$name ]; then
+		dokuError "L'examen $name n'existe pas"
+		return 1	
+	fi
+	
+	cgiHeader
         run "$DOKU_CGI?module=$module&exam=$name&action=displayExamForm"
 	
 
