e51f154ed806e621ec6aa598ae915e4aa2d8ed64
Jean-Baptiste GRAND <jean-baptiste.grand@etu.udamail.fr>
2016-03-07 09:01:45 +0100

    Modification findQuestionByKeywords

--- dokuwiki/bin/requests/findQuestionByKeywords.sh
+++ dokuwiki/bin/requests/findQuestionByKeywords.sh
@@ -4,8 +4,8 @@ runRequest() {
 	local dokuName=find_question_by_keywords
 	local out=$DOKU_USERS_DIR/$DokuUser/$dokuName.txt
 	local module=$(param module)
-	local list=$(param list)
-	local quesionsDir=$DB_USERS_DIR/$DokuUser/$module/questionsFound
+	local keywordSearch=$(param keywordSearch)
+	local dokuUserQuestionsDir=$DOKU_USERS_DIR/$DokuUser/questions
 
 
 
@@ -15,53 +15,59 @@ runRequest() {
 
 	name="$DokuUser-$name"
 
+	
 	# Inclure Question.sh
 	source "$CODE_DIR/Question.sh"	
-	
-	QUESTIONPATH=$dokuUserQuestionsDir
-	
 
- 	#mkdir -p $examsDir/questionsFound	
 	
-	#for i in $nbQuestions; do
-		#Parcours des mots clés recherchés
-		#for j in $list; do 
-			#local keywords=$(getQuestionElement $DB_MODULES_DIR/$module/questions/$i.txt keywords)
-			#if test "$keywords" == "$j"; then
-				#cp $DB_MODULES_DIR/$module/questions/$i.txt $examsDir/questionsFound
-			#fi
-		#done
-	#done
+ 	#mkdir -p $examsDir/questionsFound/
 
-
-	echo "==== test ===" > $out
 	
-	i=$(getQuestionElement $DB_MODULES_DIR/$module/questions/10.txt duration)
-        echo $i >> $out
+#	echo "==== test ===" > $out
+	
+#	i=$(getQuestionElement $DB_MODULES_DIR/$module/questions/10.txt duration)
+#        echo $i >> $out
 
-	list="aaa bbb ccc"
-	for q in $list; do
-		echo "  * $q" >> $out
-	done
-	echo $list >> $out
+#	list="aaa bbb ccc"
+#	for q in $list; do
+#		echo "  * $q" >> $out
+#	done
+#	echo $list >> $out
 	
 
-	echo " test blabla" >> $out		
+#	echo " test blabla" >> $out		
+    
+    	QUESTIONPATH=$dokuUserQuestionsDir
+    
+    	for i in $(cd $dokuUserQuestionsDir; ls *.txt | sed -re 's/\.txt$//' | sort -n); do	
+		QUESTIONID=$i
+		
+		# Charger la question
+		mainLoadQuestion
+		
+		local temporaryKeywords=`getElement "$questionFileContents" keywords`
 	
-	cat << EOF >> $out	
-
-
-======= Questions trouvées (module $module) =======
-
-EOF
-
-	local list="$(ls $examsDir/$name/questionsFound)"
-	if test -z "$list"; then
-		echo "Aucunes questions trouvées !" >> $out
-	else
-		for l in $list; do
-			showQuestionItem $examsDir/questionsFound/$l.txt >> $out 
+		for keyword in $KEYWORDS; do
+				if test "$keyword" = "$keywordSearch"
+					echo "===== Question #$i =====" >> $out
+				fi
 		done
-	fi		
+   	done	
+	
+#	cat << EOF >> $out	
+
+
+#======= Questions trouvées (module $module) =======
+
+#EOF
+
+#	local list="$(ls $examsDir/$name/questionsFound)"
+#	if test -z "$list"; then
+#		echo "Aucunes questions trouvées !" >> $out
+#	else
+#		for l in $list; do
+#			showQuestionItem $examsDir/questionsFound/$l.txt >> $out 
+#		done
+#	fi		
 	redirect users:$DokuUser:$dokuName
 }
