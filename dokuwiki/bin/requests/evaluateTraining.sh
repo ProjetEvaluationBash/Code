af339348e56ac10d221910c880a4c7601858fad9
Pierre PIC <pierre.pic@etu.udamail.fr>
2016-03-07 09:13:05 +0100

    Correction erreur 500

--- dokuwiki/bin/requests/evaluateTraining.sh
+++ dokuwiki/bin/requests/evaluateTraining.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 
-runRequest()
+runRequest(){
 	local dokuName=evaluate_training
 	local out=$DOKU_USERS_DIR/$DokuUser/$dokuName.txt
         local module=$(param module)
@@ -13,7 +13,7 @@ runRequest()
 		local answerNum=`echo $i | cut -d: -f1`
 		local questionNum=`echo $i | cut -d: -f2`
 		local answerName="answer$answerNum"
-		answer=$(param answerName)
+		answer=$(param $answerName)
 		
 		test_to_print+=$answer
 		test_to_print+=" "
@@ -21,13 +21,9 @@ runRequest()
 	done
 
 cat << EOF > $out
-	<html>
-
-		$test_to_print
-
-
-	</html>
-
+	==== $temp ====
+	$test_to_print
 EOF
-	
+	cgiHeader	
 	redirect users:$DokuUser:$dokuName
+}
