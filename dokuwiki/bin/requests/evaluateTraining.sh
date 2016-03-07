d03dfe51f83fc3464e436f1f26beca7959c20c89
Florian MOUSSE <florian.mousse@etu.udamail.fr>
2016-03-07 11:55:37 +0100

    "ajout de la fonction permettant d'evaluer le formulaire"

--- dokuwiki/bin/requests/evaluateTraining.sh
+++ dokuwiki/bin/requests/evaluateTraining.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+source "$CODE_DIR/Question.sh"
+evaluateAnswer() {
+	if test $# -ne 3; then
+                echo "Usage: MCQ : EvalAnswer ANSWER" >&2
+                return 0
+        fi
+
+	QUESTIONPATH=$1
+	QUESTIONID=$2
+	mainLoadQuestion
+	isCorrect = dokuwikiEvaluateAnswer $3 
+	if test $isCorrect -eq 0 ; then
+		 return 1
+	fi
+	return 0
+
+}
 
 runRequest(){
 	local dokuName=evaluate_training
@@ -10,7 +27,9 @@ runRequest(){
 	local testid=$(param testid)
 	local testsDir=$DB_USERS_DIR/$DokuUser/$module/tests/$testid
 	local answerDir=$testsDir/answers
-		
+	local questiondir=$testsDir/questions
+	local score=0
+	
 	test_to_print=" "
 
 	for i in $list; do
@@ -21,14 +40,17 @@ runRequest(){
 		echo $answer >> $answerDir/$questionNum.txt
 		test_to_print+=$answer
 		test_to_print+=" "
-
+		pts=`evaluateAnswer $questiondir  $questionNum  $answer`		
+		$score = $(($score + $pts))
+		
 	done
 
 cat << EOF > $out
 	==== $temp ====
 	$test_to_print
 	$answerDir
+	$score
 EOF
-		
+	#run "$DOKU_CGI?module=$module&action=displayTrainingAnswer"	
 	redirect users:$DokuUser:$dokuName
 }
